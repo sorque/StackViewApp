36992650446a3e30678fa549d545ecc8bf7fc31e
sorque <michvl@yandex.ru>
2023-05-10 00:46:42 +0300

    Initial Commit

---
+++ StackViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewApp
+//
+//  Created by m.korovin on 10.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
